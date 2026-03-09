d8504a3374ef01d5f83a83a8e33770181442b45b
RohiniLongadge14 <156320261+RohiniLongadge14@users.noreply.github.com>
2026-03-09 20:50:59 +0530

    Clean up eslint.config.js formatting

--- eslint.config.js
+++ eslint.config.js
@@ -12,11 +12,7 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
-    ]
-
-
-
-    
+    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
